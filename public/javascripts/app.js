b00defb3b9fe8e50d8e4b2f056b05f16ec50ecad
ruichao-factual <ruichao@structuredcommons.com>
2013-08-09 10:45:03 +0800

    display points on the map

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,8 +1,8 @@
 var elevator;
 var map;
+var markers = [];
 //var infowindow = new google.maps.InfoWindow();
 //var denali = new google.maps.LatLng(34.041242, -118.254927);
-var denali = new google.maps.LatLng(40.085632, -75.393583);
 
 
 var $lat = $("input.lat");
@@ -11,23 +11,69 @@ var $alt = $("input.alt");
 
 var $searchForm = $("#searchForm");
 
+elevator = new google.maps.ElevationService();
 function initialize() {
-  var mapOptions = {
-    zoom: 16,
-    center: denali,
-    mapTypeId: 'terrain'
-  }
-  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-  elevator = new google.maps.ElevationService();
-  google.maps.event.addListener(map, 'click', getElevation);
+    var denali = new google.maps.LatLng(40.085632, -75.393583);
+    var mapOptions = {
+        zoom: 16,
+        center: denali,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    var map = new google.maps.Map($('#map-canvas')[0], mapOptions);
+    google.maps.event.addListener(map, 'click', getElevation);
 
-
-  $searchForm.ajaxForm({
-    success: function (rows) {
-               console.log(rows);
+    $searchForm.ajaxForm({
+        success: function (response, status) {
+            deleteOverlays();
                //showEntities(rows);
-             }
-  });
+            $.each(response,function(index, value){
+                var entityLatlng = new google.maps.LatLng(value.latitude,value.longitude);
+                var contentString = '<p><h2>'+value.name+'</h2><br /><b>Address</b>:'+value.address+
+                    '<br /><b>Tel</b>:'+value.telphone
+                var title = value.name;
+                addMarker(entityLatlng, contentString, title);
+            })
+            setAllMap(map);
+        }
+    });
+}
+
+// Add a marker to map and push to the array.
+function addMarker(location, contentString, title){
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString
+    });
+    var marker = new google.maps.Marker({
+        position: location,
+        map: map,
+        title: title
+    });
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.open(map,marker);
+    });
+    markers.push(marker);
+}
+
+// Set the map on all markers in the array.
+function setAllMap(map) {
+    for (var i =0; i < markers.length; i++) {
+        markers[i].setMap(map);
+    }
+}
+
+// Remove the overlays from the map, but keeps them in the array.
+function clearOverLays(){
+    setAllMap(null);
+}
+
+// Shows any overlays currently in the array.
+function showOverlays(){
+    setAllMap(map);
+}
+// Delete all markers in the array by removing references to them.
+function deleteOverlays() {
+    clearOverLays();
+    markers = [];
 }
 
 function getElevation(event) {
